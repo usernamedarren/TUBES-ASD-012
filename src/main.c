3c1d0a8e59cf74da86eb4fcc0591d99706f0352f
usernamedarren <19623168@std.stei.itb.ac.id>
2024-11-25 16:04:39 +0700

    [Refactor] Use isKataEqual instead of strcmp

--- src/main.c
+++ src/main.c
@@ -34,12 +34,12 @@ int main()
             printf("Masukkan command (kata langsung/angka): ");
             char command[100];
             input(command);
-            if (strcmp(command,"START")==0||strcmp(command,"start")==0||strcmp(command,"1")==0)
+            if (isKataEqual(command, "START") || isKataEqual(command,"start") || isKataEqual(command,"1"))
             {
                 start(&B,&U);
                 menu=2;
             }
-            else if (strcmp(command,"LOAD")==0||strcmp(command,"load")==0||strcmp(command,"2")==0)
+            else if (isKataEqual(command,"LOAD") || isKataEqual(command,"load") || isKataEqual(command,"2"))
             {
                 char filename[100];
                 printf("Masukkan nama file: ");
@@ -52,12 +52,12 @@ int main()
                     printf("File konfigurasi aplikasi berhasil dibaca. PURRMART berhasil dijalankan.\n");}
                 else menu=1;
             }
-            else if (strcmp(command,"QUIT")==0||strcmp(command,"quit")==0||strcmp(command,"3")==0)
+            else if (isKataEqual(command,"QUIT") || isKataEqual(command,"quit") || isKataEqual(command,"3"))
             {
                 quit(B,U);
                 break;
             } 
-            else if (strcmp(command,"HELP")==0||strcmp(command,"help")==0) 
+            else if (isKataEqual(command,"HELP") || isKataEqual(command,"help")) 
             {
                 clearterminal();
                 Help(menu);
@@ -74,11 +74,11 @@ int main()
             printf("Masukkan command (kata langsung/angka): ");
             char command[100];
             input(command);
-            if (strcmp(command,"REGISTER")==0||strcmp(command,"register")==0||strcmp(command,"1")==0)
+            if (isKataEqual(command,"REGISTER") || isKataEqual(command,"register") || isKataEqual(command,"1"))
             {
                 registeruser(&U);
             }
-            else if (strcmp(command,"LOGIN")==0||strcmp(command,"login")==0||strcmp(command,"2")==0)
+            else if (isKataEqual(command,"LOGIN") || isKataEqual(command,"login") || isKataEqual(command,"2"))
             {
                 login(&U,&user_id);
                 if (user_id!=-1) {
@@ -86,12 +86,12 @@ int main()
                 }
                 else menu=2;
             }
-            else if (strcmp(command,"QUIT")==0||strcmp(command,"quit")==0||strcmp(command,"3")==0)
+            else if (isKataEqual(command,"QUIT") || isKataEqual(command,"quit") || isKataEqual(command,"3"))
             {
                 quit(B,U);
                 break;
             }
-            else if (strcmp(command,"HELP")==0||strcmp(command,"help")==0) 
+            else if (isKataEqual(command,"HELP") || isKataEqual(command,"help")) 
             {
                 clearterminal();
                 Help(menu);
@@ -108,49 +108,49 @@ int main()
             printf("Masukkan command (kata langsung/angka): ");
             char command[100];
             input(command);
-            if (strcmp(command,"WORK")==0||strcmp(command,"work")==0||strcmp(command,"1")==0)
+            if (isKataEqual(command,"WORK") || isKataEqual(command,"work") || isKataEqual(command,"1"))
             {
                 work(user_id,&U);
             }
-            else if (strcmp(command,"WORK CHALLENGE")==0||strcmp(command,"work challenge")==0||strcmp(command,"2")==0)
+            else if (isKataEqual(command,"WORK CHALLENGE") || isKataEqual(command,"work challenge") || isKataEqual(command,"2"))
             {
                 workChallenge(user_id,&U);
             }
-            else if (strcmp(command,"STORE LIST")==0||strcmp(command,"store list")==0||strcmp(command,"3")==0)
+            else if (isKataEqual(command,"STORE LIST") || isKataEqual(command,"store list") || isKataEqual(command,"3"))
             {
                 listStore(B);
             }
-            else if (strcmp(command,"STORE REQUEST")==0||strcmp(command,"store request")==0||strcmp(command,"4")==0)
+            else if (isKataEqual(command,"STORE REQUEST") || isKataEqual(command,"store request") || isKataEqual(command,"4"))
             {
                 requestStore(B,&Q);
             }
-            else if (strcmp(command,"STORE SUPPLY")==0||strcmp(command,"store supply")==0||strcmp(command,"5")==0)
+            else if (isKataEqual(command,"STORE SUPPLY") || isKataEqual(command,"store supply") || isKataEqual(command,"5"))
             {
                 supplyStore(&B,&Q);
             }
-            else if (strcmp(command,"STORE REMOVE")==0||strcmp(command,"store remove")==0||strcmp(command,"6")==0)
+            else if (isKataEqual(command,"STORE REMOVE") || isKataEqual(command,"store remove") || isKataEqual(command,"6"))
             {
                 removeStore(&B);
             }
-            else if (strcmp(command,"BIO WEAPON")==0||strcmp(command,"bio weapon")==0||strcmp(command,"7")==0)
+            else if (isKataEqual(command,"BIO WEAPON") || isKataEqual(command,"bio weapon") || isKataEqual(command,"7"))
             {
                 bioweapon(&Q,B);
             }
-            else if (strcmp(command,"LOGOUT")==0||strcmp(command,"logout")==0||strcmp(command,"8")==0)
+            else if (isKataEqual(command,"LOGOUT") || isKataEqual(command,"logout") || isKataEqual(command,"8"))
             {
                 logout(&user_id,U);
                 menu=2;
             }
-            else if (strcmp(command,"SAVE")==0||strcmp(command,"save")==0||strcmp(command,"9")==0)
+            else if (isKataEqual(command,"SAVE") || isKataEqual(command,"save") || isKataEqual(command,"9"))
             {
                 Save(B,U);
             }
-            else if (strcmp(command,"QUIT")==0||strcmp(command,"quit")==0||strcmp(command,"10")==0)
+            else if (isKataEqual(command,"QUIT") || isKataEqual(command,"quit") || isKataEqual(command,"10"))
             {
                 quit(B,U);
                 break;
             } 
-            else if (strcmp(command,"HELP")==0||strcmp(command,"help")==0) 
+            else if (isKataEqual(command,"HELP") || isKataEqual(command,"help")) 
             {
                 clearterminal();
                 Help(menu);
